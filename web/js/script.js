1cf0cae1ffd7d565302e7e8f443e4c82cd1502e8
alisson youssf <youssfbr@gmail.com>
2020-12-28 15:55:15 -0300

    Formulario - Envio de E-mail

--- web/js/script.js
+++ web/js/script.js
@@ -14,3 +14,14 @@ if (window.SimpleSlide) {
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+if (window.SimpleForm) {
+  new SimpleForm({
+    form: '.formphp', // seletor do formulário
+    button: '#enviar', // seletor do botão
+    erro:
+      "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para p email contato.bikcraft.alisson@gmail.com</p></div>",
+    sucesso:
+      "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>",
+  });
+}
